58cc07c1928d6688a3bd431d3c2404c6d5f92d66
Martin Becerra <mrtnbcrr@gmail.com>
2018-09-07 14:48:44 -0300

    Added alertmanger configurations

---
+++ alertmanager/send_alerts.sh
@@ -0,0 +1,15 @@
+alerts1='[
+  {
+    "labels": {
+       "alertname": "DiskRunningFull",
+       "dev": "sda1",
+       "instance": "example1"
+     },
+     "annotations": {
+        "info": "The disk sda1 is running full",
+        "summary": "please check the instance example1"
+      }
+  }
+]'
+
+curl -XPOST -d"$alerts1" http://localhost:9093/api/v1/alerts
